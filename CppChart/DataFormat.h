71ece6e175909e297f3b2896b2f6c57c2288929e
Chris Ohk <utilForever@gmail.com>
2016-05-13 00:00:32 +0900

    Implement struct DataFormat

--- CppChart/DataFormat.h
+++ CppChart/DataFormat.h
@@ -1,4 +1,18 @@
 #ifndef DATA_FORMAT_H
 #define DATA_FORMAT_H
 
+#include "SFML/Graphics.hpp"
+
+namespace CppChart
+{
+	struct DataFormat
+	{
+		DataFormat(const std::string& _name, const sf::Color& _color) :
+			name(_name), color(_color) { }
+
+		std::string name;
+		sf::Color color;
+	};
+}
+
 #endif

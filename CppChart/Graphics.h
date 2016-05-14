bc226d68cf3d37cd71d17a48005c35da9b8a9c58
Chris Ohk <utilForever@gmail.com>
2016-05-14 22:35:58 +0900

    Declare class SectorShape

--- CppChart/Graphics.h
+++ CppChart/Graphics.h
@@ -34,6 +34,44 @@ namespace CppChart
 	void DrawDottedLine(sf::RenderTarget*, const sf::Vector2f&, const sf::Vector2f&, const sf::Color&, float = 3.0f, float = 1.0f);
 	void SetTextAtCenter(sf::Text&, float, float, float, float);
 	sf::CircleShape PlotPoint(float, float, float, const sf::Color&);
+
+	class SectorShape : public sf::Transformable, public sf::Drawable
+	{
+	public:
+		SectorShape() { }
+		SectorShape(float, float, const sf::Vector2f& = sf::Vector2f(0.0f, 0.0f));
+
+		void SetCenter(const sf::Vector2f& c)
+		{
+			m_center = c;
+			Update();
+		}
+
+		void SetFillColor(const sf::Color& c)
+		{
+			m_fillColor = c;
+			Update();
+		}
+
+		void Rotate(float a)
+		{
+			LogFnStart();
+			Update(a);
+			LogFnEnd();
+		}
+
+		virtual void Draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	private:
+		void Update(float = 0.0f);
+
+		sf::Vector2f m_center;
+		float m_radius;
+		float m_angle;
+		sf::VertexArray m_pie;
+		sf::Color m_fillColor;
+		unsigned int m_points;
+	};
 }
 
 #endif

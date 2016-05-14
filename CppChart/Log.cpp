08f39e836746ef184ffe7265ec118d0d9246987e
Chris Ohk <utilForever@gmail.com>
2016-05-14 15:38:13 +0900

    Implement operator<<, ToString()

--- CppChart/Log.cpp
+++ CppChart/Log.cpp
@@ -1,6 +1,32 @@
+#include <sstream>
+
 #include "Log.h"
 
 namespace CppChart
 {
-	
+	std::ostream& operator<<(std::ostream& stream, const sf::Vector2f& v)
+	{
+		stream << "(" << v.x << ", " << v.y << ")";
+		return stream;
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const sf::Vector2u& v)
+	{
+		stream << "(" << v.x << ", " << v.y << ")";
+		return stream;
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const sf::Color& c)
+	{
+		stream << "(" << static_cast<int>(c.r) << ", " << static_cast<int>(c.g) << "," << static_cast<int>(c.b) << ")";
+		return stream;
+	}
+
+	template <typename T>
+	std::string ToString(const T& val)
+	{
+		std::ostringstream oss;
+		oss << val;
+		return oss.str();
+	}
 }

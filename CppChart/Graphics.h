295a767f65b49c4786062fc478791e959536df06
Chris Ohk <utilForever@gmail.com>
2016-05-14 22:10:53 +0900

    Delcare some functions

--- CppChart/Graphics.h
+++ CppChart/Graphics.h
@@ -29,6 +29,11 @@ namespace CppChart
 		HORIZONTAL,
 		VERTICAL
 	};
+
+	sf::RectangleShape MakeRect(sf::Vector2f, sf::Vector2f, float = 1.0f);
+	void DrawDottedLine(sf::RenderTarget*, const sf::Vector2f&, const sf::Vector2f&, const sf::Color&, float = 3.0f, float = 1.0f);
+	void SetTextAtCenter(sf::Text&, float, float, float, float);
+	sf::CircleShape PlotPoint(float, float, float, const sf::Color&);
 }
 
 #endif

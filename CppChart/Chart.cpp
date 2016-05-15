3b3a408df5584999f0c1749ca2ea767149807a52
Chris Ohk <utilForever@gmail.com>
2016-05-16 00:34:18 +0900

    Implement Chart::DrawAxes()

---
+++ CppChart/Chart.cpp
@@ -0,0 +1,25 @@
+#include "Chart.h"
+
+namespace CppChart
+{
+	void Chart::DrawAxes()
+	{
+		LogFnStart();
+
+		sf::RectangleShape xAxis, yAxis;
+		xAxis.setFillColor(m_axes.color);
+		yAxis.setFillColor(m_axes.color);
+
+		// Y-axis
+		yAxis.setPosition(sf::Vector2f(m_chartOffsets.x / 2.0f, m_chartOffsets.y / 2.0f - m_axes.labels.fontSize));
+		yAxis.setSize(sf::Vector2f(m_axes.thickness, m_chartHeight - m_chartOffsets.y));
+		m_chartTexture.draw(yAxis);
+
+		// X-axis
+		xAxis.setPosition(sf::Vector2f(m_chartOffsets.x / 2.0f, m_chartHeight - m_chartOffsets.y / 2.0f - m_axes.thickness - m_axes.labels.fontSize));
+		xAxis.setSize(sf::Vector2f(m_chartWidth - m_chartOffsets.x, m_axes.thickness));
+		m_chartTexture.draw(xAxis);
+
+		LogFnEnd();
+	}
+}

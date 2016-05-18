1975faccbd1cc2b1ea695c211940094752874ad4
Chris Ohk <utilForever@gmail.com>
2016-05-18 12:34:55 +0900

    Declare class MultiBarChart

--- CppChart/Bar.h
+++ CppChart/Bar.h
@@ -25,6 +25,28 @@ namespace CppChart
 		float m_width;
 		float m_vSize;
 	};
+
+	class MultiBarChart : public Chart
+	{
+	public:
+		MultiBarChart(const std::vector<std::vector<float>>&, const std::vector<std::string>&, const std::vector<sf::Color>&);
+
+		void Render() override;
+
+		float m_gap;
+		unsigned int m_guides;
+		bool m_displayGuides;
+		bool m_displayValues;
+
+	protected:
+		std::vector<std::vector<float>> m_multiData;
+		std::vector<std::string> m_axesLabels;
+		std::vector<sf::Color> m_barColors;
+
+		float m_max;
+		float m_width;
+		float m_vSize;
+	};
 }
 
 #endif

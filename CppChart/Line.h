6b80226d416dfe5e987a7dfa3bb925e1953f482a
Chris Ohk <utilForever@gmail.com>
2016-05-24 00:43:48 +0900

    Declare class LineChart's public part

--- CppChart/Line.h
+++ CppChart/Line.h
@@ -1,9 +1,25 @@
 #ifndef LINE_H
 #define LINE_H
 
+#include "Chart.h"
+
 namespace CppChart
 {
-	
+	class LineChart : public Chart
+	{
+	public:
+		LineChart() = default;
+		LineChart(const std::vector<DataElement>&);
+
+		void Render() override;
+
+		sf::Color m_anchorColor, m_guideColor, m_lineColor, m_fillColor;
+		float m_anchorSize, m_lineThickness;
+
+		bool m_displayGuides;
+		bool m_displayValues;
+		bool m_fill;
+	};
 }
 
 #endif
